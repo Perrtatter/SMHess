582b25a3cd4c16ec45de5ed89f9b5b43d5af9842
Quentyn.Etter <159697218+UserQuentyn@users.noreply.github.com>
2026-02-16 22:33:32 +0100

    Add Strapi

---
+++ SMHess_strapi/smhess/src/api/message-table/routes/message-table.ts
@@ -0,0 +1,7 @@
+/**
+ * message-table router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::message-table.message-table');
---
+++ SMHess_strapi/smhess/src/api/message-table/services/message-table.ts
@@ -0,0 +1,7 @@
+/**
+ * message-table service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::message-table.message-table');
